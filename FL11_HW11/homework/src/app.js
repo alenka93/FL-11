cbf9ab074adc54fb98e28484667a04b7ae1a2834
Alena Kozlova <dp010193kas@gmail.com>
2019-07-23 18:48:47 +0300

    Add files via upload

---
+++ FL11_HW11/homework/src/app.js
@@ -0,0 +1,29 @@
+// let rootNode = document.getElementById("root");
+
+let list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if(ev.target.tagName === 'LI') {
+       ev.target.classList.toggle('checked');
+    } else if(ev.target.tagName === 'SPAN') {
+       let div = ev.target.parentNode;
+       div.remove();
+    }
+}, false);
+
+function newElement() {
+    let li = document.createElement('li');
+    let inputValue = document.getElementById('toDoEl').value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue === '') {
+       alert('Add new action');
+    } else {
+       document.getElementById('list').appendChild(li);
+    }
+    document.getElementById('toDoEl').value = '';
+    let span = document.createElement('SPAN');
+    let txt = document.createTextNode('X');
+    span.className = 'close';
+    span.appendChild(txt);
+    li.appendChild(span);
+}// Your code goes here
